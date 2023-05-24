1b097a4e3cac44f3af8a46ce3dd60de61c62ed89
JamesB0907 <virverum22@gmail.com>
2023-05-24 09:10:58 -0400

    die roller done

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,11 @@
-// Put your code here
+
 
 const names = [
    "Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("'The' Place Names");
 
-for (const name of names) {
-  console.log(name);
-}
+const filteredNames = names.filter(name => name.startsWith("The"))
+filteredNames.forEach(name => console.log(name))
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
-// Put your code here
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  const numNames = ["one", "two", "three", "four", "five", "six"]
+  
+  console.log("Let's roll some dice, baby!")
+  console.log("---------------------------")
+  
+  const dieRoll = Array.from({ length: 10 }, () => {
+    const die1 = getRandomInt(1, 6)
+    const die2 = getRandomInt(1, 6)
+    const die1string = numNames[die1 - 1]
+    const die2string = numNames[die2 - 1]
+    return `${die1string} + ${die2string} = ${die1 + die2}${(die1 === die2) ? " DOUBLES!" : ""}`;
+  });
+  
+  dieRoll.forEach(result => console.log(result))
+  
